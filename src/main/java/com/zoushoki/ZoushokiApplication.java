1f7b030c2dce0436368626938b285054a31470e4
Joilton Ramalho dos Santos <joilton7santos@hotmail.com>
2025-10-19 13:34:25 -0300

    feat: Adiciona estrutura inicial do projeto

---
+++ src/main/java/com/zoushoki/ZoushokiApplication.java
@@ -0,0 +1,13 @@
+package com.zoushoki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoushokiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoushokiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoushoki/ZoushokiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoushoki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoushokiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
